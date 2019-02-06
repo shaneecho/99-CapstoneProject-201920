a54198e511e0a70bd7a83f61d7f93b10acd495dc
yans2 <yans2@rose-hulman.edu>
2019-02-06 16:40:44 -0500

    finish testing raise arm

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -16,13 +16,27 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
-    run_test_raisearm()
+    run_test_raise_arm()
+    run_test_calibrate_arm()
+    run_test_move_arm_to_position()
+    run_test_lower_arm()
 
 
-def run_test_raisearm():
+def run_test_raise_arm():
     robot = rosebot.RoseBot()
     robot.arm_and_claw.raise_arm()
 
+def run_test_calibrate_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
+
+def run_test_move_arm_to_position():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.move_arm_to_position()
+
+def run_test_lower_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.lower_arm()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
