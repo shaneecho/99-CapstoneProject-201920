8721f5eb55eb0b66bcb32a76f76892ee4631d332
yans2 <yans2@rose-hulman.edu>
2019-02-06 16:52:08 -0500

    finish testing code

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -16,10 +16,10 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
-    run_test_raise_arm()
+    # run_test_raise_arm()
     run_test_calibrate_arm()
-    run_test_move_arm_to_position()
-    run_test_lower_arm()
+    # run_test_move_arm_to_position()
+    # run_test_lower_arm()
 
 
 def run_test_raise_arm():
