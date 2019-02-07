f89258dfacf00cea5fb73e40237c8e04fe2454b4
Hanrui Chen <chenh8@rose-hulman.edu>
2019-02-07 15:57:58 -0500

    gui finished

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -165,6 +165,10 @@ def handle_forward(left_entry_box, right_entry_box, mqtt_sender):
       :type  mqtt_sender:      com.MqttClient
     """
 
+    print("Forward", left_entry_box.get(), right_entry_box.get())
+    mqtt_sender.send_message("forward", left_entry_box.get(), right_entry_box.get())
+
+
 
 def handle_backward(left_entry_box, right_entry_box, mqtt_sender):
     """
--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -7,3 +7,11 @@
   Winter term, 2018-2019.
 """
 
+class Receiver(object):
+    def __init__(self,robot):
+        """ :type robot: rosebot.Rosebot"""
+        self.robot = robot
+
+    def forward(self,left_wheel_speed, right_wheel_speed):
+        print("Got forward", left_wheel_speed, right_wheel_speed)
+        self.robot.drive_system.go(int(left_wheel_speed), int(right_wheel_speed))
